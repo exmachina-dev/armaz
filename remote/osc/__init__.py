a78e1b2c3d8a898f48f97b8a3441d62c3e5871b3
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-01-15 19:18:56 +0100

    New class OSCCommands
    Handle setup messages (set, get)

--- remote/osc/__init__.py
+++ remote/osc/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from .osc import OSCServer
+from .osc import OSCCommands as OSCServer
--- remote/osc/osc.py
+++ remote/osc/osc.py
@@ -1,6 +1,7 @@
 #!/usr/bin python3
 # -*- coding: utf-8 -*-
 
+import configparser
 import liblo as lo
 
 class OSCServer(lo.ServerThread):
@@ -34,6 +35,61 @@ class OSCServer(lo.ServerThread):
 
     def send(self, dst, msg):
         super(OSCServer, self).send(lo.Address(dst.get_hostname(), self.client_port), msg)
+
+class OSCCommands(OSCServer):
+    """
+    OSCCommands contains all commands available thru OSCServer.
+    """
+    _commands_store = {
+
+    }
+
+    def setup_reply(self, sender, *args):
+        _msg = lo.Message('/setup/return', *args)
+        self.send(sender, _msg)
+
+    @lo.make_method('/setup/set', 'ssi')
+    @lo.make_method('/setup/set', 'ssh')
+    @lo.make_method('/setup/set', 'ssf')
+    @lo.make_method('/setup/set', 'ssd')
+    @lo.make_method('/setup/set', 'ssc')
+    @lo.make_method('/setup/set', 'sss')
+    @lo.make_method('/setup/set', 'ssS')
+    @lo.make_method('/setup/set', 'ssm')
+    @lo.make_method('/setup/set', 'ssT')
+    @lo.make_method('/setup/set', 'ssF')
+    @lo.make_method('/setup/set', 'ssN')
+    @lo.make_method('/setup/set', 'ssI')
+    @lo.make_method('/setup/set', 'ssb')
+    def setup_set_callback(self, path, args, types, sender):
+        setup_sec, setup_opt, args, = args
+        print(self.config.sections())
+
+        try:
+            self.config.set(setup_sec, setup_opt, str(args))
+            self.setup_reply(sender, setup_sec, setup_opt, True)
+        except configparser.NoOptionError as err:
+            self.setup_reply(sender, setup_sec, str(err))
+        except configparser.NoSectionError as err:
+            self.setup_reply(sender, str(err))
+        except:
+            self.setup_reply(sender, str(ValueError))
+
+        print(path, setup_sec, setup_opt, args, types, sender)
+        return 0
+
+    @lo.make_method('/setup/get', 'ss')
+    def setup_get_callback(self, path, args, types, sender):
+        if len(args) != 2:
+            return 1
+        setup_section, setup_var = args
+        try:
+            args.append(self.config.get(setup_section, setup_var))
+            self.setup_reply(sender, *args)
+        except configparser.NoOptionError as err:
+            print(sender, err)
+            self.setup_reply(sender, setup_section, str(err))
+        return 0
 
     #def setConfig(self, c):
     #    for k, l in c:
