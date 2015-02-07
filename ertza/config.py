9f35889baa8e96896617916e3d0df01ceb939848
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-02-07 19:20:32 +0100

    Restart OSC Server on watched options change, not on every change.

--- ertza/config.py
+++ ertza/config.py
@@ -34,9 +34,16 @@ class ConfigWorker(BaseWorker):
         super(ConfigWorker, self).__init__(sm)
         self.get_logger()
 
+        self.watched_options = {
+                'osc': {
+                    'server_port': None
+                    },
+                }
+
         try:
             self.lg.debug('Reading configs: %s', self.cfpr.configs)
             self.cfpr.read_configs()
+            self._watchconfig(init=True)
         except configparser.Error as e:
             error = err.ConfigError(e.message)
             self.lg.warn(error)
@@ -47,7 +54,25 @@ class ConfigWorker(BaseWorker):
     def run(self):
         self.config_event.set()
         while not self.exit_event.is_set():
+            self._watchconfig()
+
             time.sleep(0.5)
+
+    def _watchconfig(self, init=None):
+        for s, o in self.watched_options.items():
+            for o, v in o.items():
+                try:
+                    if self.cfpr[s][o] is not v:
+                        if not init:
+                            if s is 'osc' and o is 'server_port':
+                                self.lg.debug("""
+Server port as changed. Old port: %s. New port: %s
+Triggering OSC event.
+                                """, v, self.cfpr[s][o])
+                                self.osc_event.set()
+                        self.watched_options[s][o] = self.cfpr[s][o]
+                except configparser.Exception as e:
+                    self.lg.debug(repr(e))
 
 
 class ConfigProxy(configparser.ConfigParser):
@@ -69,11 +94,6 @@ class ConfigProxy(configparser.ConfigParser):
         return super(ConfigProxy, self).get(section, line, fallback=fallback)
 
     def set(self, section, option, value=None):
-        if section is 'osc' and option is 'server_port':
-            if self['osc']['server_port'] is not value:
-                self.lg.info('OSC server port has changed… Restarting.')
-                self.osc_event.set()
-
         rtn = super(ConfigProxy, self).set(section, option, value)
         self.save()
 
