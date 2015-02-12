4b7ee75b02b7898b28357d8d4420d2491e4c75ad
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-02-12 13:19:02 +0100

    Updated test_config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,7 +4,7 @@ from ertza import config
 
 class Test_ConfigRequest(object):
     def setup_class(self):
-        self.rq = config.ConfigRequest(None)
+        self.rq = config.ConfigRequest(config.ConfigParser)
 
     def test_get(self):
         assert self.rq.get('str', 'str', 0) == 0
