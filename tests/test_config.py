7eb35a2d830a4cd710eab7fc56a42ac718563949
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-02-14 13:27:37 +0100

    Use FakeConfig in tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,10 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from ertza import config
+import pytest
+import configparser
+
+from ertza.config import ConfigRequest, _DEFAULTS
+from ertza.utils import FakeConfig
 
 class Test_ConfigRequest(object):
     def setup_class(self):
-        self.rq = config.ConfigRequest(None)
+        self.rq = ConfigRequest(FakeConfig())
 
     def test_get(self):
-        assert self.rq.get('str', 'str', 0) == 0
+        assert int(self.rq.get('osc', 'server_port', 0)) == \
+                _DEFAULTS['osc']['server_port']
+
+        assert self.rq.get('fake_section', 'fake_option') == None
+
+    def test_set(self):
+        with pytest.raises(configparser.NoSectionError):
+            self.rq.set('str', 'str', 0)
+
+        with pytest.raises(ValueError):
+            self.rq.set('osc', 0)
+
+        with pytest.raises(ValueError):
+            self.rq.set('str', 'str')
