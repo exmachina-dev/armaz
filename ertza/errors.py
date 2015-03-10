68158b5952e8256c203592fe8229434a29470e08
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-03-10 23:03:30 +0100

    Errors can log if logger is passed as argument.

--- ertza/errors.py
+++ ertza/errors.py
@@ -17,47 +17,50 @@ class ErtzaError(Exception):
 class FatalError(ErtzaError):
     """Raised when program cannot continue."""
 
-    def __init__(self, msg=''):
-        Error.__init__(self, 'Fatal: %s' % (msg,))
+    def __init__(self, msg='', lg=None):
+        msg = 'Fatal: %s' % (msg,)
+        ErtzaError.__init__(self, msg)
+        if lg:
+            lg.error(msg)
 
 
 class TimeoutError(ErtzaError):
     """Raised on a function timeout."""
 
-    def __init__(self, msg=''):
-        Error.__init__(self, 'Timeout: %s' % (msg,))
+    def __init__(self, msg='', lg=None):
+        ErtzaError.__init__(self, 'Timeout: %s' % (msg,), lg)
 
 
 class ConfigError(FatalError):
     """Raised when program cannot load config."""
 
-    def __init__(self, msg=''):
-        FatalError.__init__(self, 'Unable to load config: %s' % (msg,))
+    def __init__(self, msg='', lg=None):
+        FatalError.__init__(self, 'Unable to load config: %s' % (msg,), lg)
 
 
 class OSCServerError(FatalError):
     """Raised when program cannot start OSC server."""
 
-    def __init__(self, msg=''):
-        FatalError.__init__(self, 'Cannot start OSC server: %s' % (msg,))
+    def __init__(self, msg='', lg=None):
+        FatalError.__init__(self, 'Cannot start OSC server: %s' % (msg,), lg)
 
 
 class SlaveError(FatalError):
     """Raised in Slave server."""
 
-    def __init__(self, msg=''):
-        FatalError.__init__(self, 'Slave error: %s' % (msg,))
+    def __init__(self, msg='', lg=None):
+        FatalError.__init__(self, 'Slave error: %s' % (msg,), lg)
 
 
 class SerialError(FatalError):
     """Raised when program cannot start OSC server."""
 
-    def __init__(self, msg=''):
-        FatalError.__init__(self, 'SerialError: %s' % (msg,))
+    def __init__(self, msg='', lg=None):
+        FatalError.__init__(self, 'SerialError: %s' % (msg,), lg)
 
 
 class ModbusMasterError(FatalError):
     """Raised when program cannot start Modbus master."""
 
-    def __init__(self, msg=''):
-        Error.__init__(self, 'Cannot start Modbus master: %s' % (msg,))
+    def __init__(self, msg='', lg=None):
+        FatalError.__init__(self, 'Cannot start Modbus master: %s' % (msg,), lg)
