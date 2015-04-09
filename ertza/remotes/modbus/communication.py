15d899a4cbad8ea37cd9a33c21ea10c48ef40533
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-04-09 08:59:32 +0000

    Fixed old code

--- ertza/remotes/modbus/communication.py
+++ ertza/remotes/modbus/communication.py
@@ -98,7 +98,7 @@ class ModbusResponse(BaseResponse):
         args = self.request.args
         try:
             if self.request.method & self._methods['set']:
-                self.set_to_config(*args)
+                self.set_to_device(*args)
             elif self.request.method & self._methods['get']:
                 self.get_from_device(*args)
             elif self.request.method & self._methods['dmp']:
