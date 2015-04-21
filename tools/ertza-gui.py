971a6133f235c77b14b16b7b446b7b358752ddaa
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-04-21 15:41:25 +0000

    Debug GUI updated

--- tools/ertza-gui.py
+++ tools/ertza-gui.py
@@ -37,6 +37,13 @@ class ErtzaOSCServer(lo.ServerThread):
     def online_cb(self, path, args, types, sender):
         self.to.print('%s is online, listening on %s' % (
             sender.get_hostname(), args[0]))
+        if self.to.to['dev_addr'] == sender.get_hostname():
+            self.reply('/setup/dump', sender)
+            self.reply('/motor/status', sender)
+
+    @lo.make_method(None, None)
+    def default_cb(self, path, args, types, sender):
+        self.to.handle(sender.get_hostname(), path, *args)
 
 
 class ErtzaActions(object):
@@ -55,6 +62,16 @@ class ErtzaActions(object):
 
         self.master.ctl_drive_enable.set('Drive enable')
 
+        self.fields = {
+                'osc': {
+                    'server_port': self.master.conf_osc_server_port,
+                    'client_port': self.master.conf_osc_client_port,
+                    },
+                'control': {
+                    'mode': self.master.conf_ctrl_mode,
+                    },
+                }
+
         self.print('Welcome to Ertza debug GUI.', 'Version : %s' % (VERSION,))
 
     def print(self, *msg, **kwargs):
@@ -67,6 +84,30 @@ class ErtzaActions(object):
         else:
             for m in msg:
                 self.master.log_list.insert(index, m)
+
+    def handle(self, sender, path, *args):
+        if self.master.dbg_state:
+            self.print('From %s : %s %s' % (sender, path, args))
+
+        if '/status/motor/' in path:
+            if '/drive_temperature' in path:
+                pass
+            elif '/error_code' in path:
+                pass
+            elif '/enable_input' in path:
+                pass
+            elif '/enable_ready' in path:
+                pass
+            elif '/enable' in path:
+                pass
+            elif '/brake' in path:
+                pass
+        elif '/setup/' in path:
+            if '/get/' in path:
+                sec, opt, val = args
+            elif '/value' in path:
+                sec, opt, val = args
+                self.update_field(sec, opt, val)
 
     def connect(self):
         self.to = {
@@ -108,6 +149,12 @@ class ErtzaActions(object):
     def update_status(self):
         self.send('/motor/status')
 
+    def update_field(self, sec, opt, val):
+        try:
+            self.fields[sec][opt] = val
+        except KeyError:
+            pass
+
 
 class ErtzaGui(tk.Frame):
     def __init__(self, master):
@@ -117,7 +164,60 @@ class ErtzaGui(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.init_vars()
+        self._device_section()
+
+        self._log_section()
+
+        self._control_section()
+
+        self._config_section()
+
+        self.quit_but = tk.Button(self, text="Close",
+                command=self.actions.close)
+        self.quit_but.grid()
+
+        self.actions.load_defaults()
+
+    def _log_section(self):
+        self.log_frame = tk.Frame(self)
+        lg = self.log_frame
+
+        self.log_y_scroll = tk.Scrollbar(lg, orient=tk.VERTICAL)
+        self.log_y_scroll.grid(row=0, column=1, sticky=tk.N+tk.S)
+
+        self.log_x_scroll = tk.Scrollbar(lg, orient=tk.HORIZONTAL)
+        self.log_x_scroll.grid(row=1, column=0, sticky=tk.E+tk.W)
+
+        self.log_list = tk.Listbox(lg,
+                xscrollcommand=self.log_x_scroll.set,
+                yscrollcommand=self.log_y_scroll.set,
+                width=40)
+        self.log_list.grid(row=0, column=0,
+                sticky=tk.N+tk.S+tk.E+tk.W)
+        self.log_x_scroll['command'] = self.log_list.xview
+        self.log_y_scroll['command'] = self.log_list.yview
+
+        self.log_frame.grid(row=0, column=1)
+
+    def _control_section(self):
+        self.ctl_drive_enable = tk.StringVar()
+
+        self.ctl_frame = tk.Frame(self)
+        ct = self.ctl_frame
+
+        self.ctl_enable_but = tk.Button(ct, text='Drive enable')
+        self.ctl_enable_but.grid()
+        self.ctl_enable_but['command'] = self.actions.drive_toggle
+        self.ctl_enable_but['textvariable'] = self.ctl_drive_enable
+
+        self.ctl_frame.grid(columnspan=2)
+
+    def _device_section(self):
+        self.dbg_state = tk.IntVar()
+
+        self.dev_addr = tk.StringVar()
+        self.dev_port = tk.IntVar()
+        self.srv_port = tk.IntVar()
 
         self.dev_frame = tk.Frame(self)
         dv = self.dev_frame
@@ -139,54 +239,39 @@ class ErtzaGui(tk.Frame):
                 width=5)
         self.srv_port_entry.grid(row=r, column=6, columnspan=1)
 
+        r = 2
+        self.debug_check = tk.Checkbutton(dv, text='Debug',
+                variable=self.dbg_state)
+        self.debug_check.grid(row=r, columnspan=2)
         self.connect_but = tk.Button(dv, text='Connect',
                 command=self.actions.connect)
-        self.connect_but.grid(column=4, columnspan=3)
+        self.connect_but.grid(row=r, column=4, columnspan=3)
 
         self.dev_frame.grid()
 
-        self.log_frame = tk.Frame(self)
-        lg = self.log_frame
+    def _config_section(self):
+        self.conf_osc_server_port = tk.IntVar()
+        self.conf_osc_client_port = tk.IntVar()
 
-        self.log_y_scroll = tk.Scrollbar(lg, orient=tk.VERTICAL)
-        self.log_y_scroll.grid(row=0, column=1, sticky=tk.N+tk.S)
+        self.conf_ctrl_mode = tk.StringVar()
 
-        self.log_x_scroll = tk.Scrollbar(lg, orient=tk.HORIZONTAL)
-        self.log_x_scroll.grid(row=1, column=0, sticky=tk.E+tk.W)
+        self.config_frame = tk.Frame(self)
+        cf = self.config_frame
+        r = 0
+        self.conf_osc_server_port_label = tk.Label(cf, text='Server port')
+        self.conf_osc_server_port_label.grid(row=r, columnspan=2)
+        self.conf_osc_server_port_entry = tk.Entry(cf,
+            textvariable=self.conf_osc_server_port_label, width=15)
+        self.conf_osc_server_port_entry.grid(row=r, column=3)
 
-        self.log_list = tk.Listbox(lg,
-                xscrollcommand=self.log_x_scroll.set,
-                yscrollcommand=self.log_y_scroll.set,
-                width=40)
-        self.log_list.grid(row=0, column=0,
-                sticky=tk.N+tk.S+tk.E+tk.W)
-        self.log_x_scroll['command'] = self.log_list.xview
-        self.log_y_scroll['command'] = self.log_list.yview
+        self.conf_osc_client_port_label = tk.Label(cf, text='Client port')
+        self.conf_osc_client_port_label.grid(row=r, column=4, columnspan=2)
+        self.conf_osc_client_port_entry = tk.Entry(cf,
+            textvariable=self.conf_osc_client_port_label, width=15)
+        self.conf_osc_client_port_entry.grid(row=r, column=6)
 
-        self.log_frame.grid(row=0, column=1)
+        self.config_frame.grid()
 
-        self.ctl_frame = tk.Frame(self)
-        ct = self.ctl_frame
-
-        self.ctl_enable_but = tk.Button(ct, text='Drive enable')
-        self.ctl_enable_but.grid()
-        self.ctl_enable_but['command'] = self.actions.drive_toggle
-        self.ctl_enable_but['textvariable'] = self.ctl_drive_enable
-
-        self.ctl_frame.grid(columnspan=2)
-
-        self.quit_but = tk.Button(self, text="Close",
-                command=self.actions.close)
-        self.quit_but.grid()
-
-        self.actions.load_defaults()
-
-    def init_vars(self):
-        self.dev_addr = tk.StringVar()
-        self.dev_port = tk.IntVar()
-        self.srv_port = tk.IntVar()
-
-        self.ctl_drive_enable = tk.StringVar()
 
 
 root = tk.Tk()
