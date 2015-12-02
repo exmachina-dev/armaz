14d314b3ce3d8d3a218f58d15570748e35e78374
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-12-02 16:37:25 +0000

    Better handling of ModbusDriver.connect()

--- ertza/drivers/Modbus.py
+++ ertza/drivers/Modbus.py
@@ -4,18 +4,25 @@ from drivers.AbstractDriver import AbstractDriver
 
 from drivers.ModbusBackend import ModbusBackend
 
+from drivers.Utils import retry
+
+
+class ModbusDriverError(Exception):
+    pass
 
 class ModbusDriver(AbstractDriver):
 
     def __init__(self, config):
 
         self.target_address = config.get("target_address")
-        self.target_port = config.get("target_port")
+        self.target_port = int(config.get("target_port"))
         self.target_nodeid = '.'.split(self.target_address)[-1]
 
         self.back = ModbusBackend(self.target_address, self.target_port,
                                   self.target_nodeid)
 
+    @retry(ModbusDriverError, 5, 5, 2)
     def connect(self):
-
-        self.back.connect()
+        if not self.back.connect():
+            raise ModbusDriverError("Failed to connect %s:%i" % (self.target_address,
+                                                                 self.target_port))
---
+++ ertza/drivers/Utils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import time
+
+
+def retry(ExceptionToCatch, tries=3, wait=5, backoff=2):
+    def decorator_retry(f):
+        def f_retry(*args, **kwargs):
+            mtries, mwait, mbackoff = tries, wait, backoff
+            while tries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCatch as e:
+                    logging.exception("Retrying in %d..." % mwait)
+
+                    time.sleep(mwait)
+                    mtries -= 1
+                    mwait *= mbackoff
+
+            return f(*args, **kwargs)
+        return f_retry
+    return decorator_retry
