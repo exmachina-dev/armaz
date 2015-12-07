e31c9a4ae8a0676b0421c2858ed0f223de42c3fc
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-12-07 19:15:15 +0100

    New class OscPath: handle path easily Distinct receiver vs sender

--- ertza/Osc.py
+++ ertza/Osc.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
 
 
+class OscPath(str):
+    def __init__(self, path):
+        self._p = path
+        self.levels = self._p.split('/')
+
+    def __repr__(self):
+        return "%s" % '/'.join(self.levels)
+
+    def __str__(self):
+        return "%s" % '/'.join(self.levels)
+
+
 class OscMessage(object):
 
-    def __init__(self, path, args, types, sender):
-        self.path, self.args, self.types = path, args, types
-        self.sender = sender
+    def __init__(self, path, args, **kwargs):
+        self.path, self._args = OscPath(path), args
+        self.sender, self.receiver = None, None
+
+        if 'types' in kwargs:
+            self.types = kwargs['types']
+
+        if 'sender' in kwargs:
+            self.sender = kwargs['sender']
+        if 'receiver' in kwargs:
+            self.receiver = kwargs['receiver']
+
+        self.answer = None
+        self.protocol = 'OSC'
 
     @property
     def target(self):
@@ -15,6 +38,11 @@ class OscMessage(object):
     def action(self):
         return self.path.split('/')[-1]
 
-    def values(self):
-        for a in self.args:
+    @property
+    def args(self):
+        for a in self._args:
             yield a
+
+    def __repr__(self):
+        return '%s: %s %s' % (self.__class__.__name__, self.path,
+                              ' '.join(iter(self.args)))
