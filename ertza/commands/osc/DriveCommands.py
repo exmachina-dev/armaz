56b963cdc201fb8d8824ded14b2e804c837257af
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-12-09 16:47:47 +0100

    Clean up in OSC commands

--- ertza/commands/osc/DriveCommands.py
+++ ertza/commands/osc/DriveCommands.py
@@ -1,35 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from ertza.commands.AbstractCommands import UnbufferedCommand
+from ertza.commands.AbstractCommands import BufferedCommand
 from ertza.commands.OscCommand import OscCommand
 
 from ertza.Osc import OscMessage
 
 
-class DriveStatus(OscCommand):
+class DriveStatus(OscCommand, BufferedCommand):
 
     def execute(self, c):
         msg = OscMessage('/drive/status', ('ok',), receiver=c.sender)
         self.machine.send_message(c.protocol, msg)
-
-
-class ListCommands(OscCommand, UnbufferedCommand):
-
-    def execute(self, c):
-        cmds = self.machine.osc_processor.available_commands
-        reply_path = '/help/implemented_osc_commands'
-        for cmd in cmds:
-            msg = OscMessage(reply_path, cmd, receiver=c.sender)
-            self.machine.send_message(c.protocol, msg)
-
-    def get_description(self):
-        return "List all implemented G-codes"
-
-    def get_long_description(self):
-        return ("Lists all the G-codes implemented by this firmware. "
-                "To get a long description of each code use '?' "
-                "after the code name, for instance, G0? will give a long decription of G0")
-
-    @property
-    def alias(self):
-        return '/help'
---
+++ ertza/commands/osc/HelpCommands.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from ertza.commands.AbstractCommands import UnbufferedCommand
+from ertza.commands.OscCommand import OscCommand
+
+from ertza.Osc import OscMessage
+
+
+class ListCommands(OscCommand, UnbufferedCommand):
+
+    def execute(self, c):
+        cmds = self.machine.osc_processor.available_commands
+        reply_path = '/help/implemented_osc_commands'
+        for cmd in cmds:
+            msg = OscMessage(reply_path, (cmd,), receiver=c.sender)
+            self.machine.send_message(c.protocol, msg)
+
+    @property
+    def alias(self):
+        return '/help/list'
