259556bf0b99c65bd343b44d3aa4f0e03e1078f6
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-12-10 12:50:14 +0100

    New modbus API: driver['key'] get a value, driver['key'] = val set it.

--- ertza/drivers/Modbus.py
+++ ertza/drivers/Modbus.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from collections import namedtuple
+
 from ertza.drivers.AbstractDriver import AbstractDriver
 
 from ertza.drivers.ModbusBackend import ModbusBackend
@@ -11,22 +13,143 @@ class ModbusDriverError(Exception):
     pass
 
 
+class ReadOnlyError(ModbusDriverError, IOError):
+    def __init__(self, key):
+        super().__init__('%s is read-only' % key)
+
+
+class WriteOnlyError(ModbusDriverError, IOError):
+    def __init__(self, key):
+        super().__init__('%s is write-only' % key)
+
+
 class ModbusDriver(AbstractDriver):
+
+    netdata = namedtuple('netdata', ['addr', 'fmt'])
+    parameter = namedtuple('parameter', ['netdata', 'start',
+                                         'vtype', 'mode'])
+    mfNdMap = {
+        'status':               netdata(0,
+                                        'bool,bool,bool,bool,bool,bool'),
+        'command':              netdata(1,
+                                        'bool,bool,bool,bool,uint:2,uint:1,bool'),
+        'error_code':           netdata(2, 'uint:32'),
+        'jog':                  netdata(3, 'float:32'),
+        'torque_ref':           netdata(4, 'float:32'),
+        'velocity_ref':         netdata(5, 'float:32'),
+        'position_ref':         netdata(6, 'float:32'),
+
+        'torque_rise_time':     netdata(10, 'float:32'),
+        'torque_fall_time':     netdata(11, 'float:32'),
+        'acceleration':         netdata(12, 'float:32'),
+        'deceleration':         netdata(13, 'float:32'),
+
+        'velocity':             netdata(21, 'float:32'),
+        'position':             netdata(22, 'float:32'),
+        'position_target':      netdata(23, 'float:32'),
+        'position_remaining':   netdata(24, 'float:32'),
+        'encoder_ticks':        netdata(25, 'float:32'),
+        'encoder_velocity':     netdata(26, 'float:32'),
+        'velocity_error':       netdata(27, 'float:32'),
+        'follow_error':         netdata(28, 'float:32'),
+        'effort':               netdata(29, 'float:32'),
+
+        'drive_temp':           netdata(30, 'float:32'),
+        'dropped_frames':       netdata(31, 'uint:32'),
+    }
+
+    microFlexMap = {
+        'drive_ready':      parameter(mfNdMap['status'], 0, bool, 'r'),
+        'drive_enable':     parameter(mfNdMap['status'], 1, bool, 'r'),
+        'drive_input':      parameter(mfNdMap['status'], 2, bool, 'r'),
+        'motor_brake':      parameter(mfNdMap['status'], 3, bool, 'r'),
+        'motor_temp':       parameter(mfNdMap['status'], 4, bool, 'r'),
+        'timeout':          parameter(mfNdMap['status'], 5, bool, 'r'),
+
+        'enable_command':   parameter(mfNdMap['command'], 0, bool, 'w'),
+        'cancel_command':   parameter(mfNdMap['command'], 1, bool, 'w'),
+        'clear_errors':     parameter(mfNdMap['command'], 2, bool, 'w'),
+        'reset_command':    parameter(mfNdMap['command'], 3, bool, 'w'),
+        'control_mode':     parameter(mfNdMap['command'], 4, int, 'w'),
+        'move_mode':        parameter(mfNdMap['command'], 5, int, 'w'),
+        'go_command':       parameter(mfNdMap['command'], 6, bool, 'w'),
+
+        'error_code':       parameter(mfNdMap['error_code'], 0, int, 'r'),
+        'jog':              parameter(mfNdMap['jog'], 0, float, 'rw'),
+        'torque_ref':       parameter(mfNdMap['torque_ref'], 0, float, 'rw'),
+        'velocity_ref':     parameter(mfNdMap['velocity_ref'], 0, float, 'rw'),
+        'position_ref':     parameter(mfNdMap['velocity_ref'], 0, float, 'w'),
+        'torque_rise_time': parameter(mfNdMap['torque_rise_time'], 0, float, 'rw'),
+        'torque_fall_time': parameter(mfNdMap['torque_fall_time'], 0, float, 'rw'),
+        'acceleration':     parameter(mfNdMap['acceleration'], 0, float, 'rw'),
+        'deceleration':     parameter(mfNdMap['deceleration'], 0, float, 'rw'),
+
+        'velocity':         parameter(mfNdMap['velocity'], 0, float, 'r'),
+        'position':         parameter(mfNdMap['position'], 0, float, 'r'),
+        'position_target':  parameter(mfNdMap['position_target'], 0, float, 'r'),
+        'position_remaining':
+        parameter(mfNdMap['position_remaining'], 0, float, 'r'),
+        'encoder_ticks':    parameter(mfNdMap['encoder_ticks'], 0, float, 'r'),
+        'encoder_velocity': parameter(mfNdMap['encoder_velocity'], 0, float, 'r'),
+        'velocity_error':   parameter(mfNdMap['velocity_error'], 0, float, 'r'),
+        'follow_error':     parameter(mfNdMap['follow_error'], 0, float, 'r'),
+        'effort':           parameter(mfNdMap['effort'], 0, float, 'r'),
+
+        'drive_temp':       parameter(mfNdMap['drive_temp'], 0, int, 'r'),
+        'dropped_frames':   parameter(mfNdMap['dropped_frames'], 0, int, 'r'),
+    }
 
     def __init__(self, config):
 
         self.target_address = config.get("target_address")
         self.target_port = int(config.get("target_port"))
-        self.target_nodeid = '.'.split(self.target_address)[-1]
+        self.target_nodeid = '.'.split(self.target_address)[-1]     # On MFE100, nodeid is always the last byte of his IP address
 
         self.back = ModbusBackend(self.target_address, self.target_port,
                                   self.target_nodeid)
 
+        self.netdata_map = ModbusDriver.microFlexMap
+
     @retry(ModbusDriverError, 5, 5, 2)
     def connect(self):
         if not self.back.connect():
-            raise ModbusDriverError("Failed to connect %s:%i" % (self.target_address,
-                                                                 self.target_port))
+            raise ModbusDriverError(
+                "Failed to connect %s:%i" % (self.target_address,
+                                             self.target_port))
 
     def exit(self):
         self.back.close()
+
+    def __getitem__(self, key):
+        if key not in self.netdata_map:
+            raise KeyError
+
+        if 'r' not in self.netdata_map[key].mode:
+            raise ReadOnlyError(key)
+
+        nd = self.netdata_map[key].netdata
+        start = self.netdata_map[key].start
+        vtype = self.netdata_map[key].vtype
+        res = self.back.read_netdata(nd.addr, nd.fmt)
+        return vtype(res[start])
+
+    def __setitem__(self, key, value):
+        if key not in self.netdata_map:
+            raise KeyError
+
+        if 'w' not in self.netdata_map[key].mode:
+            raise WriteOnlyError(key)
+
+if __name__ == "__main__":
+    c = {
+        'target_address': 'localhost',
+        'target_port': 501,
+    }
+
+    d = ModbusDriver(c)
+
+    try:
+        d['velocity'] = True    # This shoud raise ReadOnlyError
+        print(d['position_ref'])    # This shoud raise WriteOnlyError
+    except ModbusDriverError:
+        pass
