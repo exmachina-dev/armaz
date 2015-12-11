11a66c6b81b43ed6847908f422c7ada336f84301
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-12-11 19:29:25 +0100

    Log incoming identify commands

--- ertza/commands/serial/IdentifyCommands.py
+++ ertza/commands/serial/IdentifyCommands.py
@@ -11,6 +11,8 @@ from ertza.processors.serial.Serial import SerialMessage
 class Identify(SerialCommand, BufferedCommand):
 
     def execute(self, c):
+        infos = (self.c.args, self.c.data['serial_number'],)
+        logging.info('Found %s %s with S/N %s' % infos)
         msg = SerialMessage(('identify', 'Armaz:%s:%s' % (
             self.machine.config.variant, self.machine.config.revision)))
         self.machine.send_message(c.protocol, msg)
