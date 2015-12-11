f1738b61d6aba24fb43a76cdd5ba60fbf9c7a9a8
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-12-11 20:48:10 +0100

    Many bugfixes + feature added in SerialCommandString

--- ertza/processors/serial/Serial.py
+++ ertza/processors/serial/Serial.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from bitstring import BitString
+import bitstring as bs
 from collections import namedtuple
 
 
 class SerialCommandString(object):
     CmdStruct = namedtuple('SerialCmd', ('protocol', 'serial_number', 'data', 'end'))
-    CmdFormat = 'bytes:8,bytes:12,pad:32,bytes,bytes:1'
+    CmdFormat = 'bits:64,bits:96,pad:32,bits,bits:16'
     CmdEnd = b'\r\n'
+    CmdSep = b':'
 
     def __init__(self, cmd_bytes=None, **kwargs):
         if cmd_bytes:
-            self._b = BitString(cmd_bytes, self.CmdFormat)
+            self._b = bs.pack(self.CmdFormat, cmd_bytes)
             self._c = self.CmdStruct(self._b.unpack())
         else:
-            self._c = self.CmdStruct('', '', '', '')
-            self['serial_number'] = '000000000000'
+            self._c = self.CmdStruct(b'', b'', b'', b'')
+            self['serial_number'] = '000000000000'.encode()
             self['end'] = self.CmdEnd
             if 'protocol' in kwargs:
                 self['protocol'] = kwargs['protocol']
@@ -24,21 +25,38 @@ class SerialCommandString(object):
 
     @property
     def tobytes(self):
-        return BitString(self._c, self.CmdFormat).tobytes()
+        return bs.pack(self.CmdFormat, *self._c)
 
     @property
     def command(self):
-        return self['data'].split(':')[0]
+        return self['data'].split(self.CmdSep)[0]
 
     @property
     def args(self):
-        return tuple(self['data'].split(':')[1:])
+        return tuple(self['data'].decode().split(':')[1:])
 
     def __getitem__(self, key):
         return getattr(self._c, key)
 
     def __setitem__(self, key, value):
+        if type(value) == str:
+            value = value.encode()
+
         self._c = self._c._replace(**{key: value})
+
+    def __add__(self, value):
+        if type(value) == str:
+            value = bs.Bits(value.encode())
+        elif type(value) == int:
+            value = bs.Bits(int=value, length=16)
+        elif type(value) == float:
+            value = bs.Bits(float=value, length=32)
+
+        self['data'] += self.CmdSep + value
+        return self
+
+    def __len__(self):
+        return self.tobytes.len
 
     def __repr__(self):
         return "%s %s %s" % (self['protocol'], self['serial_number'], self['data'])
@@ -84,8 +102,9 @@ class SerialMessage(object):
     def args(self):
         return self.cmd_bytes.args
 
+    @property
     def tobytes(self):
-        return self.cmd_str.tobytes
+        return self.cmd_bytes.tobytes
 
     def __repr__(self):
         return '%s: %s' % (self.__class__.__name__, self.cmd_bytes)
