b7846d541690d2eaa721657b38b401ed17f49657
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-12-11 17:59:08 +0100

    First serial command: identify

---
+++ ertza/commands/serial/IdentifyCommands.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from ertza.commands.AbstractCommands import BufferedCommand
+from ertza.commands.SerialCommand import SerialCommand
+
+from ertza.processors.Serial import SerialMessage
+
+
+class Identify(SerialCommand, BufferedCommand):
+
+    def execute(self, c):
+        msg = SerialMessage(('identify', 'Armaz:%s:%s' % (
+            self.machine.config.variant, self.machine.config.revision)))
+        self.machine.send_message(c.protocol, msg)
+
+    @property
+    def alias(self):
+        return 'identify'
