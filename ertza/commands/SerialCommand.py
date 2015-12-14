8bc7345619b661ce844e024f008bccf23ed545e0
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-12-14 15:53:22 +0100

    Added SerialCommand shortcuts (send(), ok(), error())

--- ertza/commands/SerialCommand.py
+++ ertza/commands/SerialCommand.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from ertza.processors.serial.Serial import SerialMessage
 from .AbstractCommands import AbstractCommand
 
 
@@ -10,5 +11,13 @@ class SerialCommand(AbstractCommand):
         # Processor seems to include this class while loading modules…
         return ':'
 
-    def execute(self, message):
-        target, action, args = message.target, message.action, message.args
+    def send(self, data_bytes, **kwargs):
+        m = SerialMessage()
+        m.cmd_bytes['data'] = data_bytes
+        self.machine.send_message(m)
+
+    def ok(self, command, *args, **kwargs):
+        self.send(self.alias + ':ok', **kwargs)
+
+    def error(self, command, *args, **kwargs):
+        self.send(self.alias + ':error', **kwargs)
