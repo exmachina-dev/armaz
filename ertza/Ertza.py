bcdf1639cc5f9584a004e4cf52efbd18b6174080
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-12-16 16:45:06 +0100

    Wrong attribute

--- ertza/Ertza.py
+++ ertza/Ertza.py
@@ -199,7 +199,6 @@ class Ertza(object):
 
         for f in self.machine.fans:
             f.set_value(0)
-            f.exit()
 
         for l in self.machine.leds:
             l.set_trigger('default-on')
