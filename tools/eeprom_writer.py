8972867b4d60a9b6e8086196b7b0d253893eee91
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-12-19 21:23:43 +0100

    Added tools/eeprom_writer.py
    This tool write eeprom on cape according to EEPROM format found in the
    BeagleBone SRM.

---
+++ tools/eeprom_writer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import datetime
+from collections import OrderedDict
+
+
+eeprom = '/sys/bus/i2c/devices/2-0054/eeprom'
+
+def serial_gen():
+    isodate = datetime.date.today().isocalendar()
+    yy = str(isodate[0])[2:]
+    ww = str(isodate[1]).rjust(2, '0')
+    asco = 'ARCP'
+    nnnn = input('Unit number manufactured in the current week: ').rjust(4, '0')
+
+    sn = ww + yy + asco + nnnn
+
+    return sn
+
+rev = input('Revision number (up to 4 chars)? ')
+if rev and len(rev) > 4:
+    raise ValueError('Revision number is too long')
+
+data = OrderedDict()
+data['eeprom_header'] = b'\xAA\x55\x33\xEE'
+data['eeprom_rev'] = 'A1'.encode()
+data['name'] = 'ArmazCape - Rev {}'.format(rev).ljust(32).encode()
+data['revision'] = rev.rjust(4, '0').encode() if rev else '0000'.encode()
+data['manufacturer'] = 'ExMachina'.ljust(16).encode()
+data['partnumber'] = 'ARMAZCAPE'.ljust(16).encode()
+data['nb_pins_used'] = b'\x00\x5C'
+data['serialnumber'] = serial_gen().encode()
+
+with open(eeprom, 'wb') as e:
+    for d in data.items():
+        e.write(d)
