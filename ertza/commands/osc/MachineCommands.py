270ed5f887b2538829e4ad686d9e671b5a734c06
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2015-12-20 20:19:12 +0100

    Added base OSC commands for slave handling

---
+++ ertza/commands/osc/MachineCommands.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from ertza.commands.AbstractCommands import UnbufferedCommand
+from ertza.commands.OscCommand import OscCommand
+
+
+class ListSlaves(OscCommand, UnbufferedCommand):
+
+    def execute(self, c):
+        if not self.machine.slaves:
+            self.error(c, 'Machine has no slaves')
+            return
+
+        slaves = [s.slave for s in self.machine.slaves]
+
+        self.reply(c, *slaves)
+
+    @property
+    def alias(self):
+        return '/machine/slaves'
+
+
+class AddSlave(OscCommand, UnbufferedCommand):
+
+    def execute(self, c):
+        if len(c.args) != 2:
+            self.error(c, 'Invalid number of arguments for %s' % self.alias)
+            return
+
+        try:
+            driver, address = c.args
+            n_slave = self.machine.add_slave(driver, address)
+            self.ok(c, n_slave.driver, n_slave.address, n_slave.serialnumber)
+        except Exception as e:
+            self.error(c, e)
+
+    @property
+    def alias(self):
+        return '/machine/add_slave'
