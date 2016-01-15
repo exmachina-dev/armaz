ab6e021b7d3d0a7aa0291c8167c8c22f6cf5e9dd
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2016-01-15 16:22:00 +0100

    Bugfix

--- ertza/machine/MachineModes.py
+++ ertza/machine/MachineModes.py
@@ -17,28 +17,28 @@ class AbstractMachineMode(object):
 
     @classmethod
     def _check_read_access(key):
-        StandaloneMachineMode._check_access(key)
-        if 'r' not in StandaloneMachineMode.MachineMap[key].mode:
+        AbstractMachineMode._check_access(key)
+        if 'r' not in AbstractMachineMode.MachineMap[key].mode:
             raise KeyError('{} is not readable'.format(key))
 
     @classmethod
     def _check_write_access(key):
-        StandaloneMachineMode._check_access(key)
-        if 'w' not in StandaloneMachineMode.MachineMap[key].mode:
+        AbstractMachineMode._check_access(key)
+        if 'w' not in AbstractMachineMode.MachineMap[key].mode:
             raise KeyError('{} is not writable'.format(key))
 
     @classmethod
     def _check_key(key):
-        if key not in StandaloneMachineMode.MachineMap.keys():
+        if key not in AbstractMachineMode.MachineMap.keys():
             raise KeyError('{} not in StandaloneMachineMode keys'.format(key))
 
     def __getitem__(self, key):
         key = key.split(':', maxsplit=1)[1] if key.startswith('machine:') else key
-        self._check_read_access(key)
+        AbstractMachineMode._check_read_access(key)
         return self._machine.getitem(key)
 
     def __setitem__(self, key, value):
-        self._check_write_access(key)
+        AbstractMachineMode._check_write_access(key)
         self._machine.setitem(key, self.MachineMap[key].vtype(value))
 
 
