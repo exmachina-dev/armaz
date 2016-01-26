575e4db1fb63148fa081792042fcfc79c48fadbd
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2016-01-26 20:07:22 +0100

    Parse bytes to Python types

--- ertza/commands/serial/MachineCommands.py
+++ ertza/commands/serial/MachineCommands.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import struct
+
 from ertza.commands.SerialCommand import SerialCommand
 
 
 class MachineSet(SerialCommand):
+    _uint_keys = (
+        'machine:command:control_mode',
+    )
+    _float_keys = (
+        'machine:torque_ref',
+        'machine:velocity_ref',
+        'machine:position_ref',
+        'machine:acceleration',
+        'machine:deceleration',
+    )
+    _bool_keys = (
+        'machine:command:enable',
+        'machine:command:cancel',
+        'machine:command:set_home',
+    )
 
     def execute(self, c):
         if len(c.args) < 2:
@@ -13,6 +30,13 @@ class MachineSet(SerialCommand):
         try:
             k, v, = c.args
             nk = k.decode().replace('.', ':')
+            if nk in self._float_keys:
+                v = struct.unpack('f', v)[0]
+            elif nk in self._uint_keys:
+                v = struct.unpack('H', v)[0]
+            elif nk in self._bool_keys:
+                v = struct.unpack('?', v)[0]
+
             self.machine[nk] = v
             self.ok(c, k, v)
         except Exception as e:
