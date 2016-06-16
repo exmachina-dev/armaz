20d9778c726829a2b5a68f5fa0caeabc128f0e4c
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2016-06-16 12:25:09 +0200

    New kwargs add_path in Command.reply() [DRY]

--- ertza/commands/osc_command.py
+++ ertza/commands/osc_command.py
@@ -5,19 +5,30 @@ from .abstract_commands import AbstractCommand
 
 
 class OscCommand(AbstractCommand):
+    SEP = '/'
+
     def send(self, target, path, *args, **kwargs):
         if args and isinstance(args[0], (list, tuple)):
             args = args[0]
 
         m = OscMessage(path, *args, receiver=target, **kwargs)
-        self.machine.send_message(m)
+        return self.machine.send_message(m)
 
     def ok(self, command, *args, **kwargs):
-        self.send(command.sender, self.alias + '/ok', *args, **kwargs)
+        return self.reply(command, *args, add_path='/ok', **kwargs)
 
     def error(self, command, *args, **kwargs):
         args = [str(a) if isinstance(a, Exception) else a for a in args]
-        self.send(command.sender, self.alias + '/error', *args, **kwargs)
+        return self.reply(command, *args, add_path='/error', **kwargs)
 
     def reply(self, command, *args, **kwargs):
-        self.send(command.sender, self.alias, *args, **kwargs)
+        add_path = kwargs.pop('add_path', None)
+        if add_path:
+            if not isinstance(add_path, str):
+                raise TypeError('add_path kwarg must be a string')
+            full_path = self.alias + add_path if add_path.startswith(self.SEP) \
+                else '{0.alias}{0.SEP}{1}'.format(self, add_path)
+        else:
+            full_path = self.alias
+
+        return self.send(command.sender, full_path, *args, **kwargs)
--- ertza/commands/serial_command.py
+++ ertza/commands/serial_command.py
@@ -5,6 +5,8 @@ from .abstract_commands import AbstractCommand
 
 
 class SerialCommand(AbstractCommand):
+    SEP = '.'
+
     def send(self, *args, **kwargs):
         m = kwargs['msg'] if 'msg' in kwargs else \
             SerialMessage()
@@ -12,13 +14,22 @@ class SerialCommand(AbstractCommand):
         for d in args:
             m.cmd_bytes += d
 
-        self.machine.send_message(m)
+        return self.machine.send_message(m)
 
     def ok(self, command, *args, **kwargs):
-        self.send(self.alias + '.ok', *args, **kwargs)
-
-    def reply(self, command, *args, **kwargs):
-        self.send(self.alias + '.reply', *args, **kwargs)
+        self.reply(command, *args, add_path='.ok', **kwargs)
 
     def error(self, command, *args, **kwargs):
-        self.send(self.alias + '.error', *args, **kwargs)
+        self.reply(command, *args, add_path='.error', **kwargs)
+
+    def reply(self, command, *args, **kwargs):
+        add_path = kwargs.pop('add_path', None)
+        if add_path:
+            if not isinstance(add_path, str):
+                raise TypeError('add_path kwarg must be a string')
+            full_path = self.alias + add_path if add_path.startswith(self.SEP) \
+                else '{0.alias}{0.SEP}{1}'.format(self, add_path)
+        else:
+            full_path = self.alias
+
+        self.send(command.sender, full_path, *args, **kwargs)
