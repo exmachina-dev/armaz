0eb1da2709a0ff1eb9c9344f7047b56556a4b783
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2016-06-16 12:21:53 +0200

    Import missing abstract classes in __init__.py

--- ertza/commands/__init__.py
+++ ertza/commands/__init__.py
@@ -1,3 +1,3 @@
-from .abstract_commands import BufferedCommand, UnbufferedCommand, SyncedCommand
+from .abstract_commands import AbstractCommand, BufferedCommand, UnbufferedCommand, SyncedCommand
 from .osc_command import OscCommand
 from .serial_command import SerialCommand
--- ertza/machine/__init__.py
+++ ertza/machine/__init__.py
@@ -1,2 +1,3 @@
+from .abstract_machine import AbstractMachine
 from .machine import Machine, MachineError, FatalMachineError
 from .slave import Slave, SlaveMachine, SlaveRequest, SlaveMachineError, FatalSlaveMachineError
