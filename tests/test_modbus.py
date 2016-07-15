2018036d4ef419ae226301775f501ac933794fe8
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2016-07-15 23:19:53 +0200

    Removed useless tests

--- tests/test_modbus.py
+++ tests/test_modbus.py
@@ -15,37 +15,3 @@ class Test_ModbusDriver(object):
     def test_get(self):
         with pytest.raises(KeyError):
             self.dr['nonexistingkey']
-
-        assert self.rq.get('fake_section', 'fake_option', 1234) == 1234
-
-    def test_set(self):
-        with pytest.raises(configparser.NoSectionError):
-            self.rq.set('str', 'str', 0)
-
-        with pytest.raises(ValueError):
-            self.rq.set('osc', 0)
-
-        with pytest.raises(ValueError):
-            self.rq.set('str', 'str')
-
-        rp = self.rq.set('osc', 'client_port', 4567)
-        assert rp == '4567'
-
-class Test_ModbusBitOperations(object):
-    def test_int(self):
-        start_int = 1564862
-        b = ModbusBackend._from_int(start_int)
-        end_int = ModbusBackend._to_int(b[0]+b[1])
-        assert start_int == end_int
-
-    def test_float(self):
-        start_float = 541.5486
-        b = ModbusBackend._from_float(start_float)
-        end_float = ModbusBackend._to_float(b[0]+b[1])
-        assert start_float == round(end_float, 4)
-
-    def test_bools(self):
-        start_bools = (True, False, True, False, False, True, False, True,) * 2
-        b = ModbusBackend._from_bools(start_bools)
-        end_bools = ModbusBackend._to_bools(b[0]+b[1])
-        assert list(start_bools) == end_bools
