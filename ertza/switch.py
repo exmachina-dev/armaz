228b7e1f4e7a71a5e8aba46980f7d2f01cbce048
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2016-08-15 11:18:36 +0200

    First attempt to use single thread switches

--- ertza/switch.py
+++ ertza/switch.py
@@ -14,9 +14,10 @@ Code modified by Benoit Rapidel:
     Ported to python 3
     Better handling of direction and hit
     Removed PRU section
+    Use a single thread
 """
 
-from threading import Thread
+from threading import Thread, Event
 import logging
 
 logging = logging.getLogger('ertza.switch')
@@ -25,55 +26,87 @@ logging = logging.getLogger('ertza.switch')
 class Switch(object):
 
     callback = None                 # Override this to get events
-    inputdev = "/dev/input/event1"  # File to listen to events
 
-    def __init__(self, key_code, name):
-        self.key_code = key_code
-        self.name = name
-        self.invert = False
-        self.function = False
-        self.hit = False
-        self.direction = None
+    def __new__(cls, inputdev):
+        cls._inputdev = inputdev
+        cls._thread = Thread(target=cls._wait_for_events)
+        cls._thread.daemon = True
+        cls._running_event = Event()
+        cls._keycodes = {}
 
-        self.t = Thread(target=self._wait_for_event)
-        self.t.daemon = True
-        self.t.start()
+        return super().__new__()
+
+    def __init__(self, keycode, name, **kwargs):
+        keycode_conf = {
+            'name': name,
+            'invert': kwargs.pop('invert', False),
+            'function': kwargs.pop('function', False),
+            'hit': kwargs.pop('hit', False),
+            'direction': kwargs.pop('direction', None),
+        }
+        self._keycodes[keycode] = keycode_conf
+        self.keycode = keycode
+
+    @classmethod
+    def start(cls):
+        if len(cls._keycodes):
+            raise ValueError('No keycodes assigned to {0.__name__}, '
+                             'cannot start.'.format(cls))
+
+        if cls.inputdev is None:
+            raise ValueError('Inputdev cannot be None.')
+
+        try:
+            cls._thread.start()
+        except RuntimeError:
+            logging.info('{0.__name__} already started.'.format(cls))
+
+    @classmethod
+    def get_key_config(cls, keycode):
+        config = cls._keycodes[keycode].copy()
+        return config
+
+    @classmethod
+    def get_key_state(cls, keycode):
+        state = cls.get_key_config(keycode)
+        state['keycode'] = keycode
+        return state
 
     @property
     def state(self):
-        s = {
-            'direction': self.direction,
-            'function': self.function,
-            'hit': self.hit,
-            'key_code': self.key_code,
-            'invert': self.invert,
-            'name': self.name,
-        }
-        return s
+        return self.get_key_state(self.keycode)
 
-    def _wait_for_event(self):
-        evt_file = open(Switch.inputdev, "rb")
-        while True:
-            evt = evt_file.read(16)     # Read the event
-            evt_file.read(16)           # Discard the debounce event
-            code = evt[10]
+    @property
+    def key_config(self):
+        return self.get_key_config(self.keycode)
 
-            if code == self.key_code:
-                self.direction = True if evt[12] else False
-                self.hit = False
+    @classmethod
+    def _wait_for_events(cls):
+        try:
+            with open(cls._inputdev, 'rb') as evt_file:
+                while not cls._running_event.is_set():
+                    evt = evt_file.read(16)     # Read the event
+                    evt_file.read(16)           # Discard the debounce event
+                    keycode = evt[10]
 
-                if self.invert is True and self.direction is True:
-                    self.hit = True
-                elif self.invert is False and self.direction is False:
-                    self.hit = True
+                    if keycode in cls._keycodes.keys():
+                        cnf = cls.get_key_config(keycode)
+                        k_dir = True if evt[12] else False
+                        k_hit = True if (cnf['invert'] and cnf['direction']) or \
+                            (not cnf['invert'] and not cnf['direction']) else False
 
-                if Switch.callback is not None:
-                    try:
-                        Switch.callback(self.state)
-                    except Exception as e:
-                        logging.warn('Exception in {!s}: {!s}'.format(self, e))
-            else:
-                self.direction = None
+                        cls._keycodes[keycode]['direction'] = k_dir
+                        cls._keycodes[keycode]['hit'] = k_hit
+
+                        if Switch.callback is not None:
+                            try:
+                                Switch.callback(cls.get_key_state(keycode))
+                            except Exception as e:
+                                logging.warn('Exception in {!s}: {!s}'.format(cls, e))
+                    else:
+                        logging.debug('Got unrecognized keycode: {}'.format(keycode))
+        except OSError as e:
+            logging.error('Unable to acces to inputdev file: {!s}'.format(e))
 
     def __repr__(self):
         i = {
