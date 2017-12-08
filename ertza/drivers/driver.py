72222508845ad25b4b58dec016b206d75976d6c5
Benoit Rapidel <benoit.rapidel@exmachina.fr>
2017-12-08 13:24:50 +0100

    Make get_driver more reliable

--- ertza/drivers/driver.py
+++ ertza/drivers/driver.py
@@ -6,4 +6,4 @@ from importlib import import_module
 class Driver(object):
     def get_driver(self, driver):
         pkg = import_module("ertza.drivers.%s" % driver.lower())
-        return getattr(pkg, "%sDriver" % driver)
+        return getattr(pkg, "%sDriver" % driver.title())
